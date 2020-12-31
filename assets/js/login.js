b8d1a5f31e51c485ee65c8f90129300f4498b9d2
yawang <191489657@qq.com>
2020-12-31 15:22:21 +0800

    完成了登录注册页面

--- assets/js/login.js
+++ assets/js/login.js
@@ -8,25 +8,45 @@ $(function () {
         $('.ct_reg').stop().hide();
     })
     // 验证密码的规则
+    var layer = layui.layer;
     var form = layui.form;
     form.verify({
         pwd: [    //密码验证
             /^[\S]{6,12}$/
-            ,'密码必须6到12位，且不能出现空格'
+            , '密码必须6到12位，且不能出现空格'
         ],
         // 此处的value就是表单用户输入的值
         repwd: value => {
-            if(value !== $('#pwd').val()){
+            if (value !== $('#pwd').val()) {
                 return '两次输入密码不一致！'
             }
         }
     })
     // 监听注册表单的提交事件
-    $('#form-reg').on('submit',function(e){
+    $('#form-reg').on('submit', function (e) {
         e.preventDefault();
         var data = $(this).serialize();
-        $.post('/api/reguser',data,res=>{
-            console.log(res);
+        // 注意 这里的api根路径 在baseAPI.js中作了拼接
+        $.post('/api/reguser', data, res => {
+            if (res.status !== 0) return layer.msg(res.message)
+            layer.msg(res.message);
+            $('#go_login').click();
+        })
+    })
+    // 监听登录表单的提交事件
+    $('#form-login').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: '/api/login',
+            data: $(this).serialize(),
+            success: res => {
+                if (res.status !== 0) return layer.msg(res.message)
+                layer.msg(res.message)
+                // 将登录服务器给的权限值保存在本地缓存
+                localStorage.setItem('token',res.token)
+                location.href = "/index.html"
+            }
         })
     })
 })
